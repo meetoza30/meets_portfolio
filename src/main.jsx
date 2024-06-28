e78aef3ca086353c1a1f8fa58a79389e7755a19f
meetoza30 <meetoz305@gmail.com>
2024-06-28 23:30:30 +0530

    Update configuration

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createHashRouter} from 'react-router-dom'
 import App from './App'
 import Home from './Components/Home'
 import About from './Components/About'
 import Contact from './Components/Contact'
-
 import { HashRouter as Router } from 'react-router-dom';
 
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
   path: '/',
   element: <App />,
